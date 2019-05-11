7f7a71e9acffd4efa6f53998e1148852618e8592
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-05-10 18:18:44 -0700

    testing/ListingBasic - exception thrown if data is null - pass

--- tests/ListingBasicTest.php
+++ tests/ListingBasicTest.php
@@ -3,4 +3,12 @@ use PHPUnit\Framework\TestCase;
 
 class ListingBasicTest extends TestCase {
 
+  public function testExceptionThrownIfNullData() {
+    $this->expectException(Exception::class);
+    $this->expectExceptionMessage("Unable to create a listing, data unavailable");
+
+    $data = [ ];
+    $listing = new ListingBasic($data);
+  }
+
 }
